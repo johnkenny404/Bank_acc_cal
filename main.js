d6aa48127f73ce6bb2e3a038b0a1966b9eaaac79
johnkenny404 <johnkenny404@gmail.com>
2022-04-23 00:40:37 +0100

    working with js

--- main.js
+++ main.js
@@ -5,11 +5,18 @@ const labelIn = document.querySelector(".summary__value--in");
 const labelOut = document.querySelector(".summary__value--out");
 const intAmout = document.querySelector(".summary__value--interest");
 
+// login
+const lognUser = document.querySelector(".login_user");
+const loginInPin = document.querySelector(".login_pin");
+const loginBTN = document.querySelector(".login-btn");
+const labelWelcomeMsg = document.querySelector(".welcome");
+const appContainer = document.querySelector(".app");
+
 const account1 = {
   owner: "Kenny John",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2,
-  pin: 2222,
+  pin: 1111,
 };
 
 const account2 = {
@@ -50,7 +57,7 @@ const displayMovements = function (movements) {
   });
 };
 
-displayMovements(account1.movements);
+// displayMovements(account1.movements);
 
 const calDisplayBal = function (movements) {
   const balance = movements.reduce(function (acc, mov) {
@@ -58,27 +65,27 @@ const calDisplayBal = function (movements) {
   }, 0);
   labelBalance.textContent = `${balance} EUR`;
 };
-calDisplayBal(account1.movements);
+// calDisplayBal(account1.movements);
 
-const calDisplaySummary = function (movements) {
-  const sumary = movements
+const calDisplaySummary = function (acc) {
+  const sumary = acc.movements
     .filter((mov) => mov > 0)
     .reduce((mov, accu) => mov + accu, 0);
   labelIn.textContent = `${sumary} € `;
 
-  const debitSummary = movements
+  const debitSummary = acc.movements
     .filter((mov) => mov < 0)
     .reduce((mov, accu) => mov + accu, 0);
   labelOut.textContent = `${Math.abs(debitSummary)} €`;
 
-  const int = movements
+  const int = acc.movements
     .filter((mov) => mov > 0)
-    .map((dep) => (dep * 1.2) / 100)
+    .map((dep) => (dep * acc.interestRate) / 100)
     .filter((dep) => dep >= 1)
-    .reduce((dep, acc) => dep + acc, 0);
+    .reduce((dep, ac) => dep + ac, 0);
   intAmout.textContent = `${int} €`;
 };
-calDisplaySummary(account1.movements);
+// calDisplaySummary(account1.movements);
 
 const creatUserName = function (accts) {
   accts.forEach(function (acct) {
@@ -89,3 +96,30 @@ const creatUserName = function (accts) {
       .join("");
   });
 };
+creatUserName(accounts);
+// event handlers
+let currentAccount;
+
+loginBTN.addEventListener("click", function (e) {
+  e.preventDefault();
+  currentAccount = accounts.find((acct) => acct.userName === lognUser.value);
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(loginInPin.value)) {
+    //display Ul and a welcome message
+    labelWelcomeMsg.textContent = `Welcome back, ${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    appContainer.style.opacity = 100;
+    //emptying the login field
+    loginInPin.value = lognUser.value = " ";
+    loginInPin.blur();
+    // lognUser = loginInPin = "";
+    //displayMovement
+    displayMovements(currentAccount.movements);
+    // displaybal
+    calDisplayBal(currentAccount.movements);
+    //displaysummary
+    calDisplaySummary(currentAccount);
+  }
+});
